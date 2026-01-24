e18a23c7e42a2634a73d13c2477f261f8c2eebf0
deveshcodes01 <deveshdk008@gmail.com>
2026-01-25 02:28:12 +0530

    Organizing

---
+++ JS Tutorial With Projects/Business Name Generator/index.js
@@ -0,0 +1,18 @@
+/* create a business name generator by combining list of adjectives and shop name and another word
+
+Adjectives:
+crazy
+amazing
+fire
+
+Shop name:
+engine
+foods
+garments
+
+Another word:
+bros
+limited
+hub
+
+*/
--- JS Tutorial With Projects/Faulty Calculator/faultycalc.js
+++ JS Tutorial With Projects/Faulty Calculator/faultycalc.js
@@ -10,5 +10,27 @@ This faulty calculator does following:
 / --> **
 
 It performs wrong operations 10% of times
-*/ 
+*/
 
+let random = Math.random();
+console.log(random);
+let a = prompt("enter first number");
+let c = prompt("enter operation ");
+let b = prompt("enter second number");
+
+let obj = {
+  "+": "-",
+  "*": "*",
+  "-": "/",
+  "/": "**",
+};
+
+if (random > 0.1) {
+  // perform correct calculation
+  console.log(`The Result is ${a} ${c} ${b}`)
+  alert(`the result is ${eval(`${a} ${c} ${b}`)}`);
+} else {
+  // perform wrong calculation
+  c = obj[c];
+  alert(`the result is ${eval(`${a} ${c} ${b}`)}`);
+}
