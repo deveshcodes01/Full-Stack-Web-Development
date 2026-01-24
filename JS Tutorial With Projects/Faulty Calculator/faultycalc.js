2e4f891933b6d811896753ea046446fbb202966a
deveshcodes01 <deveshdk008@gmail.com>
2026-01-24 16:02:19 +0530

    Strings and Faulty Calculator

---
+++ JS Tutorial With Projects/Faulty Calculator/faultycalc.js
@@ -0,0 +1,14 @@
+/*Create a faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. it takes two numbers as input from user
+2. it performs wrong operations as follows:
+
++ --> -
+* --> +
+- --> /
+/ --> **
+
+It performs wrong operations 10% of times
+*/ 
+
---
+++ JS Tutorial With Projects/Strings.js
@@ -0,0 +1,24 @@
+console.log("this is strings tutorial")
+let a = "Devesh";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a.length);
+
+let real_name = "Devesh";
+let friend = "rohan";
+console.log("his name is "+real_name +" and his friends name is "+friend);
+console.log(`his name is ${real_name} and his friends name is ${friend}`);
+
+let b ="shivam"
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+
+console.log(b.replace("sh","77"));
+console.log(b.concat(a,"Aishwarya","rahul","priya"));
